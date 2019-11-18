cdc5973c875df3a31da8f3da959969c8b6f39f67
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-11-18 01:55:14 +0100

    Lesson 15 - Homework, task1

--- src/task01/CountriesApp.java
+++ src/task01/CountriesApp.java
@@ -12,7 +12,6 @@ public class CountriesApp {
     public static void main(String[] args) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("countries.txt")))) {
             Map<String, Country> countryMap = getCountries(bufferedReader);
-            System.out.println(countryMap.keySet());
             printCountry(countryMap);
         } catch (IOException ex){
             System.err.println(ex.getMessage());
@@ -32,12 +31,13 @@ public class CountriesApp {
 
     }
 
-    private static void printCountry(Map<String, Country> customerMap) {
+    private static void printCountry(Map<String, Country> countryMap) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println(countryMap.keySet());
         System.out.print("Podaj id kraju: ");
         String id = scanner.nextLine();
-        if(customerMap.containsKey(id)){
-            System.out.println(customerMap.get(id));
+        if(countryMap.containsKey(id)){
+            System.out.println(countryMap.get(id));
         } else {
             System.out.println("Mapa nie zawiera kraju o takim kodzie.");
         }
