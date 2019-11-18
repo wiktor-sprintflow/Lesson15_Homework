d1541061e0ee02cd6a3163dbf37fc20059a2f773
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-11-18 23:55:27 +0100

    Lesson 15, homework - task 2

---
+++ src/task02/Vehicle.java
@@ -0,0 +1,73 @@
+package task02;
+
+class Vehicle {
+    private String type;
+    private String brand;
+    private String model;
+    private int year;
+    private int mileage;
+    private String vin;
+
+    Vehicle(String type, String brand, String model, int year, int mileage, String vin) {
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.vin = vin;
+    }
+
+    @Override
+    public String toString() {
+        return type + " " + brand + " " + model + " rocznik " + year + " przebieg " + mileage +
+                " VIN " + vin;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+}
---
+++ src/task02/VehiclesApp.java
@@ -0,0 +1,84 @@
+package task02;
+
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class VehiclesApp {
+    public static void main(String[] args) {
+        Queue<Vehicle> vehicleQueue = new LinkedList<>();
+
+        try {
+            showMenu(vehicleQueue);
+        } catch (InputMismatchException ex) {
+            System.err.println("Wprowadzono niepoprawny typ.");
+        }
+    }
+
+    private static void showMenu(Queue<Vehicle> vehicleQueue) {
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("MENU:");
+            System.out.println("0 - Zakończ działanie programu.");
+            System.out.println("1 - Dodaj nowy pojazd do kolejki");
+            System.out.println("2 - Zrób przegląd pojazdu z kolejki");
+            System.out.println("Wybierz opcję: ");
+
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 0:
+                    terminateApplication();
+                    break;
+                case 1:
+                    addNewVehicle(vehicleQueue, scanner);
+                    break;
+                case 2:
+                    inspectNextVehicle(vehicleQueue);
+                    break;
+                default:
+                    System.out.println("\nPodano niepoprawną opcję. Spróbuj ponownie. \n");
+            }
+        } while (option != 0);
+
+        scanner.close();
+    }
+
+    private static void terminateApplication() {
+        System.out.println("\nZakończenie działania aplikacji.\n");
+        System.exit(0);
+
+    }
+
+    private static void addNewVehicle(Queue<Vehicle> vehicleQueue, Scanner scanner) {
+        System.out.println("Podaj typ:");
+        String type = scanner.nextLine();
+        System.out.println("Podaj markę:");
+        String brand = scanner.nextLine();
+        System.out.println("Podaj model:");
+        String model = scanner.nextLine();
+        System.out.println("Podaj rok:");
+        int year = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Podaj przebieg:");
+        int mileage = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Podaj VIN:");
+        String vin = scanner.nextLine();
+
+        vehicleQueue.offer(new Vehicle(type, brand, model, year, mileage, vin));
+        System.out.println("\nDodano pojazd do kolejki\n");
+    }
+
+    private static void inspectNextVehicle(Queue<Vehicle> vehicleQueue) {
+        if (vehicleQueue.peek() != null) {
+            System.out.println("\nPojazd " + vehicleQueue.poll() + " pomyślnie przeszedł przeszedł przegląd.\n");
+        } else {
+            System.out.println("\nNie ma pojazdów w kolejce do przeglądu.\n");
+        }
+    }
+}
